4293cd8e88c6e3641ed974398cbd84afd4e57e32
Erik Minarini <minarini.work@gmail.com>
2016-01-31 15:39:21 +0100

    fid and 2nd table

--- Server/include/DB_Functions.php
+++ Server/include/DB_Functions.php
@@ -21,10 +21,36 @@ class DB_Functions {
      * Storing new survey
      */
     public function storeSurv($man, $prod, $dt, $lat, $lon, $fingerprint, $tag) {
+        
+
+        /*
+        INSERT INTO foo (auto,text)
+    VALUES(NULL,'text');         # generate ID by inserting NULL
+INSERT INTO foo2 (id,text)
+    VALUES(LAST_INSERT_ID(),'text');  # use ID in second table*/
+
         $stmt = $this->conn->prepare("INSERT INTO surveys(manufact, prod, dt, lat, lon, fingerprint, tag) VALUES(?, ?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("sssssss", $man, $prod, $dt, $lat, $lon, $fingerprint, $tag);
         $result = $stmt->execute();
         $stmt->close();
+        //printf ("New Record has id %d.\n", $this->conn->insert_id);
+
+        $fid = $this->conn->insert_id;
+        $rel = explode(",", $fingerprint);
+        $len = count($rel);
+
+        for($i = 0; $i<$len; $i++){
+            $input = explode(": ", $rel[$i]);
+            $stmt = $this->conn->prepare("INSERT INTO fingerprints(fid, bssid, power) VALUES(?, ?, ?)");
+            $stmt->bind_param("iss", $fid, $input[0], $input[1]);
+            $result = $stmt->execute();
+            $stmt->close();
+        }
+
+
+
+
+
  
         // check for successful store
         if ($result) {
